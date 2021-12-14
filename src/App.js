33e9c5df3141d68ce18a5dc0f3d7f6119ad2a034
alteadev <alteadev@gmail.com>
2021-12-14 19:47:10 +0100

    routes with wouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './App.css'
 import ListOfGifs from './components/ListOfGifs'
+import { Route } from 'wouter';
 
 function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs keyword='react' />
+        <a href='/'><h1>App</h1></a>
+        <a href='/gif/panda'>Gifs de Colombia</a>
+        <a href='/gif/ecuador'>Gifs de Ecuador</a>
+        <a href='/gif/chile'>Gifs de Chile</a>
+        <Route path="/gif/:keyword" component={ListOfGifs}></Route>
       </section>
     </div>
   );
--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -1,21 +1,28 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Gif from './Gif'
 import getGifs from '../services/getGifs'
 
-export default function ListOfGifs({ keyword }) {
+export default function ListOfGifs({params}) {
     const [gifs, setGifs] = useState([])
+
+    const { keyword } = params;
 
     useEffect(() => {
         getGifs({ keyword })
             .then(gifs => setGifs(gifs))
-    },[keyword])
+    }, [keyword])
 
-    return gifs.map(({id, title, url}) =>
-        <Gif 
-            key={id}
-            title={title} 
-            url={url} 
-            id={id}>
-        </Gif>
-    )
+    return <div>
+        {
+            gifs.map(({ id, title, url }) =>
+                <Gif
+                    key={id}
+                    title={title}
+                    url={url}
+                    id={id}>
+                </Gif>
+            )
+        }
+    </div>
+
 }
